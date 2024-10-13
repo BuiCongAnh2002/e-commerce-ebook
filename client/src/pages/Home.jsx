7f4e12932b4cf0cc2238f66a5fbd9482390a3b42
ThyLuu <luuthythy2003@gmail.com>
2024-10-13 16:04:39 +0700

    update

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layouts from '../Layouts/Layouts'
+import Products from '../Components/Products'
+
+const Home = () => {
+  return (
+    <Layouts>
+        <Products>
+            
+        </Products>
+    </Layouts>
+  )
+}
+
+export default Home
